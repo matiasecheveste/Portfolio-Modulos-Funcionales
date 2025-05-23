97bbe87dbe31a8382569805b0623d6d611e6b20b
Matías <nicolasecheveste5@gmail.com>
2025-05-22 23:12:11 -0300

    Update firebase.js

--- login-register-firebase/src/firebase.js
+++ login-register-firebase/src/firebase.js
@@ -1,15 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcJzQOs5RwTx0mWVJKacNc-HqU8o7mQcg",
-  authDomain: "login-register-firebase-fdfdf.firebaseapp.com",
-  projectId: "login-register-firebase-fdfdf",
-  storageBucket: "login-register-firebase-fdfdf.firebasestorage.app",
-  messagingSenderId: "648700696798",
-  appId: "1:648700696798:web:d16b0552f1cf2daea7ca4f",
-  measurementId: "G-LQGDWC05HX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
