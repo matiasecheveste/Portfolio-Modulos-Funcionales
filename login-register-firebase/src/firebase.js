26dac45d1df94fe85de665f917a7fb3b59bd0c4a
Matías <nicolasecheveste5@gmail.com>
2025-05-22 22:55:16 -0300

    Update firebase.js

--- login-register-firebase/src/firebase.js
+++ login-register-firebase/src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-console.log("Firebase config en producción:", {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -9,7 +9,7 @@ console.log("Firebase config en producción:", {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
