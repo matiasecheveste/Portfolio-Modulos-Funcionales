3373017033bd9fc983029359cdf5b2c3ac4a9283
Matías <nicolasecheveste5@gmail.com>
2025-05-22 22:57:40 -0300

    Update firebase.js

--- login-register-firebase/src/firebase.js
+++ login-register-firebase/src/firebase.js
@@ -1,14 +1,15 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCcJzQOs5RwTx0mWVJKacNc-HqU8o7mQcg",
+  authDomain: "login-register-firebase-fdfdf.firebaseapp.com",
+  projectId: "login-register-firebase-fdfdf",
+  storageBucket: "login-register-firebase-fdfdf.firebasestorage.app",
+  messagingSenderId: "648700696798",
+  appId: "1:648700696798:web:d16b0552f1cf2daea7ca4f",
+  measurementId: "G-LQGDWC05HX"
 };
 
 const app = initializeApp(firebaseConfig);
